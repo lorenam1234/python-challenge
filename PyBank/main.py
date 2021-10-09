3a4377b989e965aba83c21b35889797bbefe811a
Lorena Martinez <kaysoh@gmail.com>
2021-10-08 23:24:13 -0500

    update PyBank code

--- PyBank/main.py
+++ PyBank/main.py
@@ -3,6 +3,8 @@ import csv
 
 months_list = []
 PL_list = []
+change_list = []
+
 
 os.getcwd()
 os.chdir("/Users/lorenamartinez/UTSA_BOOT_CAMP/HOMEWORK/HW3_PythonChallenge/python-challenge/PyBank/Resources")
@@ -19,35 +21,44 @@ with open(input_data, 'r') as csv_file:
     budget_data = csv.reader(csv_file, delimiter=",")
     header = next(budget_data)
     
-    # count months
+  
     for row in budget_data:
-        months_list.append(row)
-        PL_list.append(int(row[1]))
+       # count months
+       months_list.append(row)
+       total_months = len(months_list)
+       
+       # calculate net total
+       PL_list.append(int(row[1]))
+       net_total = sum(PL_list)
+ 
+    # Calc average changes in Profit/Losses 
+    item = 0
+    prev_item = 0
+    for item in range (1,total_months):
+        if item == 0:
+            change_list.append(0)
+        else:
+            change_list.append(int(PL_list[item])-int(PL_list[prev_item]))
+            prev_item += 1
+        total_change = sum(change_list)
+        avg_change = total_change/len(change_list)
+
+    increased_profit = max(change_list)
+    decreased_profit = min(change_list)
+    increased_idx = change_list.index(increased_profit)+1
+    decreased_idx = change_list.index(decreased_profit)+1
+    increased_month = (months_list[increased_idx])
+    decreased_month = (months_list[decreased_idx])
+
+    # Display output
+    output
+    print (f"total months is {total_months}")  
+    print (f"net total is $" + str(net_total))
+    print (f"Avg change is ${round((avg_change),2)}")
+    print (f"biggest increase {increased_month[0]} $ {increased_profit}")
+    print (f"biggest decrease {decreased_month[0]} $ {decreased_profit}")
 
 
-        total_months = len(months_list)
-    print (f"total months is {total_months}")
-
-    net_total = sum(PL_list)
-    print (f"net total is ${net_total}")
-
-    # calulate net total
-    #for row in range(total_months)
-    #nettotal = nettotal +
-
-
-
-
-
-
-
-    #amount_list.append(row)
-    
-
-
-#print(total_amount)
-
+  
 
 #    df.to_csv(filename) | Write to a CSV file
-
-
